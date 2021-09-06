913051fd4938a150c42d9cd9badf9bbfbc061a56
Christopher Copeland <cmc232@zips.uakron.edu>
2021-09-06 19:09:29 -0400

    At 3.9 now

--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,20 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use crate::routes::subscribe;
-use crate::routes::health_check;
+use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-	println!("Listening on:{}",listener.local_addr().unwrap().port());
-	let server = HttpServer::new(|| {
-		App::new()
-			.route("/health_check", web::get().to(health_check))
-			.route("/subscribe", web::post().to(subscribe))
-	})
-	.listen(listener)?
-	.run();
-	Ok(server)
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+	let db_pool = web::Data::new(db_pool);
+    println!("Listening on:{}", listener.local_addr().unwrap().port());
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+            .app_data(db_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
